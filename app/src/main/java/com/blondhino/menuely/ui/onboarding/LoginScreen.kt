ea29cfbdc6e0afcb9b43e5753d591d9b42b5cb6a
eniobiondic <enio.biondic@gauss.hr>
2021-06-22 19:27:44 +0200

    fix: fixed LoginScreen crash

--- app/src/main/java/com/blondhino/menuely/ui/onboarding/LoginScreen.kt
+++ app/src/main/java/com/blondhino/menuely/ui/onboarding/LoginScreen.kt
@@ -4,7 +4,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,16 +16,18 @@ import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.navigation.NavHostController
+import androidx.navigation.NavController
 import com.blondhino.menuely.R
-import com.blondhino.menuely.data.common.NavigationRoutes.REGISTER_SCREEN
-import com.blondhino.menuely.ui.components.*
+import com.blondhino.menuely.data.common.NavigationRoutes
+import com.blondhino.menuely.ui.components.MenuelyButton
+import com.blondhino.menuely.ui.components.MenuelyCircularProgressBar
+import com.blondhino.menuely.ui.components.MenuelyTextField
 
 @Composable
 fun LoginScreen(
-    navController: NavHostController,
     viewModel: OnBoardingViewModel,
-    loading: Boolean = false,
+    navController: NavController,
+    loading: Boolean = false
 ) {
 
     Box(modifier = Modifier.fillMaxSize()) {
@@ -86,7 +89,8 @@ fun LoginScreen(
                     style = MaterialTheme.typography.h5,
                     modifier = Modifier.padding(top = 8.dp)
                 )
-                Text(stringResource(R.string.registerHere),
+                Text(
+                    stringResource(R.string.registerHere),
                     style = MaterialTheme.typography.h4,
                     modifier = Modifier
                         .padding(8.dp)
@@ -94,7 +98,7 @@ fun LoginScreen(
                             enabled = true,
                             role = Role.Button
                         ) {
-                            navController.navigate(REGISTER_SCREEN)
+                            navController.navigate(NavigationRoutes.REGISTER_SCREEN)
                         })
             }
 
@@ -117,4 +121,3 @@ fun LoginScreen(
 
 
 }
-
